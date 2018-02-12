fedfe2ef665724ed999ce3a257ddc18ab0efa436
Stallmanifold <stallmanifold@gmail.com>
2018-02-12 20:20:51 +0000

    Finish part 1 of Assignment 2 problem 1.

--- src/assignment2/cs231n/classifiers/fc_net.py
+++ src/assignment2/cs231n/classifiers/fc_net.py
@@ -47,7 +47,11 @@ class TwoLayerNet(object):
         # weights and biases using the keys 'W1' and 'b1' and second layer weights #
         # and biases using the keys 'W2' and 'b2'.                                 #
         ############################################################################
-        pass
+        self.params['W1'] = np.random.normal(loc=0.0, scale=weight_scale, size=(input_dim, hidden_dim))
+        self.params['b1'] = np.zeros(hidden_dim)
+        self.params['W2'] = np.random.normal(loc=0.0, scale=weight_scale, size=(hidden_dim,num_classes))
+        self.params['b2'] = np.zeros(num_classes)
+
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
@@ -77,7 +81,8 @@ class TwoLayerNet(object):
         # TODO: Implement the forward pass for the two-layer net, computing the    #
         # class scores for X and storing them in the scores variable.              #
         ############################################################################
-        pass
+        z1, cache1 = affine_relu_forward(X, self.params['W1'], self.params['b1'])
+        scores, cache2 = affine_forward(z1, self.params['W2'], self.params['b2'])
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
@@ -97,7 +102,22 @@ class TwoLayerNet(object):
         # automated tests, make sure that your L2 regularization includes a factor #
         # of 0.5 to simplify the expression for the gradient.                      #
         ############################################################################
-        pass
+        
+        # Get the loss and the gradient from the last layer.
+        loss, dscores = softmax_loss(scores, y)
+        
+        # Regularize the loss.
+        loss += (0.5*self.reg*np.sum(self.params['W1']*self.params['W1']) + 
+                 0.5*self.reg*np.sum(self.params['W2']*self.params['W2']))
+        
+        # Compute backpropagation to the first layer.
+        dhidden, grads['W2'], grads['b2'] = affine_backward(dscores, cache2)
+        dX, grads['W1'], grads['b1'] = affine_relu_backward(dhidden, cache1)
+
+        # Regularize the gradients.
+        grads['W1'] += self.reg*self.params['W1']
+        grads['W2'] += self.reg*self.params['W2']
+        
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
